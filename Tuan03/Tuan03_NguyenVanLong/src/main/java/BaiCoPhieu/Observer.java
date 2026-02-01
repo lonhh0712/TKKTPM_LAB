30058c11fb2da470522f8b3bea4f3a997b61ed6f
NguyeVanLong <nvlong0712@gmail.com>
2026-02-01 20:11:15 +0700

    Add tuan03

---
+++ Tuan03/Tuan03_NguyenVanLong/src/main/java/BaiCoPhieu/Observer.java
@@ -0,0 +1,5 @@
+package BaiCoPhieu;
+
+public interface Observer {
+    void update(double price);
+}
---
+++ Tuan03/Tuan03_NguyenVanLong/src/main/java/BaiCoPhieu/Subject.java
@@ -0,0 +1,22 @@
+package BaiCoPhieu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Subject {
+    protected List<Observer> observers = new ArrayList<>();
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void detach(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers(double price) {
+        for (Observer observer : observers) {
+            observer.update(price);
+        }
+    }
+}
